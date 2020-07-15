578349b187b3a65578eb2f562bdda057b5f071d1
HHeeeeeee <y40414@naver.com>
2020-07-15 21:12:39 +0900

    Q10871 시간 단축

--- src/Q10871.java
+++ src/Q10871.java
@@ -1,4 +1,27 @@
-import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Q10871 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int n = in.nextInt();
+		int x = in.nextInt();
+
+		int arr[] = new int[n];
+		
+		for (int i = 0; i < n; i++) {
+			arr[i] = in.nextInt();
+		}
+		
+		for (int j = 0; j < arr.length; j++) {
+			if (arr[j] < x) {
+				System.out.print(arr[j] + " ");
+			}
+		}
+	}
+}
+
+/*import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 public class Q10871 {
@@ -19,4 +42,4 @@ public class Q10871 {
 			System.out.print(print + " ");
 		}
 	}
-}
+}*/
