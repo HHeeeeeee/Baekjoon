760f32e95b8e4421a6e378b54cf2ac135823fa84
HHeeeeeee <y40414@naver.com>
2021-05-17 11:33:21 +0900

    refactoring - 단어의 개수

--- src/Q1152.java
+++ src/Q1152.java
@@ -1,4 +1,30 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Q1152 {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		try {
+			st = new StringTokenizer(br.readLine());
+			bw.write(st.countTokens() + "\n");
+			
+			bw.flush();
+			bw.close();
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+
+/*import java.util.Scanner;
 public class Q1152 {
 
 	public static void main(String[] args) {
@@ -16,28 +42,4 @@ public class Q1152 {
 		
 		System.out.println(word.length-count);		// 처음에 공백 기준으로 문자열 자른 문자를 배열에 넣었는데 거기서 공백의 수인 count를 뺌. 그럼 원래 문자의 개수를 알 수 있음.
 	}
-}
-
-
-/*Scanner in = new Scanner(System.in);
-String str = in.nextLine();
-int count = 0;
-
-String word[] = str.split(" ");
-count = word.length;
-
-System.out.println(count);*/
-
-
-
-/*Scanner in = new Scanner(System.in);
-
-String str = in.nextLine();
-int len = str.length();
-
-String voca[] = str.split(" ");
-
-for(int i=0; i<=len; i++){
-	System.out.println(voca[i]);
-	System.out.println(i);
 }*/
